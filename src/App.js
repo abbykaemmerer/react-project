dc76c449db82b0211c41a9a01fa886954af64e83
abbykaemmerer72 <akaemmerer@vermeer.com>
2023-10-07 18:37:55 -0500

    10/7: first pass navbar, buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" exact/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
